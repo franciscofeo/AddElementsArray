a9a0be36968835c81e9db93eff94d36e34e92a2c
Francisco Angelo <franciscoangelo.feo@gmail.com>
2020-08-09 14:10:26 -0300

    Add files via upload

---
+++ AddElements.java
@@ -0,0 +1,31 @@
+package br.com.abc.javacore.introducaometodos.classes;
+
+// With this package you can add any element (type: double) in your array!
+
+public class AddElements {
+    public static double[] addOneNum(int size, double[] arr, double element) {
+        double[] newArr = new double[size + 1];
+
+        for (int i = 0; i < size; i++) {
+            newArr[i] = arr[i];
+        }
+
+        newArr[size] = element;
+
+        return newArr;
+    }
+
+    public static double[] addNums(int size, double[] arr, double... elements) {
+        double[] newArr = new double[size + elements.length];
+
+        for(int i = 0; i < arr.length; i++){
+            newArr[i] = arr[i];
+        }
+
+        if (newArr.length - arr.length > 0)
+            System.arraycopy(elements, 0, newArr, arr.length, elements.length);
+
+        return newArr;
+
+    }
+}
